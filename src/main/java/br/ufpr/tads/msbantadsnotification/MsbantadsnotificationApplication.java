24e81fbacef1a4f54a5845616ecb728ebcd60193
mateus <imw.desenv@gmail.com>
2023-02-05 17:28:33 -0300

    Inital files

---
+++ src/main/java/br/ufpr/tads/msbantadsnotification/MsbantadsnotificationApplication.java
@@ -0,0 +1,13 @@
+package br.ufpr.tads.msbantadsnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbantadsnotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsbantadsnotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufpr/tads/msbantadsnotification/MsbantadsnotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpr.tads.msbantadsnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsbantadsnotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
